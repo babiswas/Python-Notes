14369ca0fa1a7c8b157cab05ca41d08d9a4e5805
Bapan Biswas <46675716+babiswas@users.noreply.github.com>
2019-05-12 12:48:13 +0530

    metaclass code
    metaclass code

---
+++ test123.py
@@ -0,0 +1,29 @@
+class B:
+  def __init__(self,b):
+     self.c=b
+  def cube(self):
+     print("cube method of the B class executed")
+     return self.b**3
+
+class Mymeta(type):
+   def __new__(metaclass,classname,baseclass,attrs):
+      print("New method of metaclass executed")
+      attrs['get_cube']=B.__dict__['cube']
+      return type.__new__(metaclass,classname,baseclass,attrs)
+   def __init__(self,classname,baseclass,attrs):
+      print("Init method of the metaclass executed")
+      pass
+
+class A(metaclass=Mymeta):
+   def __init__(self,a,b):
+      self.a=a
+      self.b=b
+
+a=A(2,3)
+print(a.get_cube())
+
+
+
+
+  
+
